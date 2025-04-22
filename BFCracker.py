6d9dce367cd1a1885713bc699e25c4ce3539d96d
SykesSFA <jax.f1484@gmail.com>
2025-04-21 19:06:28 -0600

    Optimized BFCracker
    * Updated BFCracker to use imap_unordered and a chunksize of 10000

--- BFCracker.py
+++ BFCracker.py
@@ -1,31 +1,24 @@
 import multiprocessing
 import itertools
 from hashlib import sha256
-import multiprocessing.managers
-from multiprocessing.managers import BaseProxy
+import functools
 
 def CrackHashSingle(_passhash: str, _minlen: int, _maxlen: int, _charset: str) -> tuple[str|None, int]:
-    chars = _charset
-    tgt_hash = _passhash
     guess_count = 0
-    for cracklen in range(_minlen, _maxlen+1):
-        print(f"Checking Passwords of length {cracklen}...")
-        guess_generator = itertools.product(chars, repeat=cracklen)
-        for guess_tup in guess_generator:
-            guess = "".join(guess_tup)
-            guess_hash = sha256(guess.encode())
-            guess_count += 1
-            if guess_hash == tgt_hash:
-                return (guess, guess_count)
+    guessgen = GuessGenerator(_charset, _minlen, _maxlen)
+    for guess in guessgen:
+        guess_count += 1
+        result = BFWorker(guess, _passhash)
+        if result is not None:
+            return (result, guess_count)
     return (None, guess_count)
     
-def BFWorker(_guess: str, _tgthash: bytes, found_event):
-    if found_event.is_set():
-        return None
+def BFWorker(_guess: str, _tgthash: bytes):
     guesshash = sha256(_guess.encode()).digest()
     if guesshash == _tgthash:
-        found_event.set()
+        print("Found Password")
         return _guess
+    return None
 
 def GuessGenerator(_charset: str, _minlen: int, _maxlen: int):
     for passlen in range(_minlen, _maxlen+1):
@@ -33,27 +26,18 @@ def GuessGenerator(_charset: str, _minlen: int, _maxlen: int):
         for guess in guess_product:
             yield "".join(guess)
 
-def CrackHashMulti(_passhash: bytes, _minlen: int, _maxlen: int, _charset: str, _processnum: int) -> tuple[str | None, int]:
-    chars = _charset
-    guess_gen = GuessGenerator(chars, _minlen, _maxlen)
+def CrackHashMulti(_passhash: bytes, _minlen: int, _maxlen: int, _charset: str) -> tuple[str | None, int]:
     guess_count = 0
-    with multiprocessing.Manager() as manager:
-        found_event = manager.Event()
-        with multiprocessing.Pool(processes=_processnum) as pool:
-            results = []
-            for guess in guess_gen:
-                if found_event.is_set():
-                    break
-                result = pool.apply_async(BFWorker, args=(guess, _passhash, found_event))
-                results.append(result)
-
-            for result in results:
-                guess_count += 1
-                res = result.get()
-                if res is not None:
-                    return (res, guess_count)
-    return (None, guess_count)
-
+    guess_gen = GuessGenerator(_charset, _minlen, _maxlen)
+    process_num = multiprocessing.cpu_count()
+    worker = functools.partial(BFWorker, _tgthash=_passhash)
+    with multiprocessing.Pool(processes=process_num) as pool:
+        for value in pool.imap_unordered(worker, guess_gen, chunksize=10000):
+            guess_count += 1
+            result = value
+            if result is not None:
+                return (result, guess_count)
+    return (result, guess_count)
 
 def GenerateCharset(_charcode: int):
     '''Takes in a 4 bit integer: 1 uses the subset, 0 doesn't use the subset'''
